45995cc194349b1c024d4d4ba0a3accf010daa39
Natalie R <tatysia@gmail.com>
2023-02-08 16:23:36 +0300

    Задача 2 о сумме элементов от М до N

---
+++ Ex2_SumMN/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Найти сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+
+int GetNumberByUser()
+{
+    Console.Write("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumMN(int numberM, int numberN)
+{
+    if (numberN == numberM) return numberM;
+    else return numberN + SumMN(numberM, numberN - 1);
+}
+
+int number1 = GetNumberByUser();
+int number2 = GetNumberByUser();
+Console.WriteLine($"Сумма элементов от {number1} до {number2} = {SumMN(number1, number2)}");
+
+
+
