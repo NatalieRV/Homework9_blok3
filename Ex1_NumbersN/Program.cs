ac6032a6b9aaaf5107de7b29a018e47561ddb9cb
Natalie R <tatysia@gmail.com>
2023-02-08 15:54:57 +0300

    Задача 1 о ряде натуральных чисел от N до 1

---
+++ Ex1_NumbersN/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значение N. Вывести все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+int GetNumberByUser()
+{
+    Console.Write("Введите натуральное число N: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string NumbersLine(int number)
+{
+    if (number >= 1) return $"{number} " + NumbersLine(number - 1);
+    else return string.Empty;
+}
+
+int num = GetNumberByUser();
+Console.WriteLine(NumbersLine(num));
